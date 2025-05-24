7cc11edef5332a4f6d009d6ae65b2845c45dc0b2
Richard <olella93@gmail.com>
2025-05-24 20:46:14 +0300

    test file updated for test_author.py

--- tests/test_author.py
+++ tests/test_author.py
@@ -1,20 +1,31 @@
 from lib.models.author import Author
 from lib.models.magazine import Magazine
-from lib.models.article import Article
 from lib.db import get_connection
 
-def test_author_save():
-    author = Author("Jane Doe")
-    author.save()
-    assert author.id is not None
-
-def test_author_articles():
-    author = Author("John Smith")
-    author.save()
-    magazine = Magazine("Tech Today", "Technology")
-    magazine.save()
+class TestAuthor:
+    """Tests for Author model (mirrors lib/models/author.py)"""
     
-    Article("Python Tips", author.id, magazine.id).save()
-    articles = author.articles()
-    assert len(articles) == 1
-    assert articles[0]['title'] == "Python Tips"
+    @classmethod
+    def setup_class(cls):
+        """Seed test data"""
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.executescript("""
+            DELETE FROM articles;
+            DELETE FROM authors;
+            INSERT INTO authors (name) VALUES ('Test Author');
+        """)
+        conn.commit()
+
+    def test_save(self):
+        author = Author("Jane Doe")
+        author.save()
+        assert author.id is not None
+
+    def test_add_article(self):
+        author = Author.find_by_id(1)
+        magazine = Magazine("Tech Weekly", "Technology")
+        magazine.save()
+        
+        author.add_article(magazine, "New Article")
+        assert len(author.articles()) == 1
